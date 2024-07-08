b08269e8197905066148e9a8b7a168cca52d8e07
Evan Walter <evwalter@deloitte.com>
2024-07-08 14:53:29 -0700

    chore: set up vite

---
+++ client-vite/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Vite + React</h1>
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+        </>
+    );
+}
+
