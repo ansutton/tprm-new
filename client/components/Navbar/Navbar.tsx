3e9e077bd0baad69205c9327cd7d6d8e9d465d92
Evan Walter <evwalter@deloitte.com>
2024-07-11 10:31:51 -0700

    feat: style Navbar

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -1,13 +1,31 @@
 export function Navbar(): JSX.Element {
     return (
-        <div className='bg-gradient-to-r from-d-green via-pink-500 to-blue-500 pb-2'>
+        <div className='bg-gradient-to-r from-d-green via-pink-500 to-blue-500 pb-1.5 shadow-md shadow-blue-400'>
             <div className='flex w-full items-center gap-2 bg-black px-3 py-6'>
-                <div className='text-xl text-white'>(Deloitte Logo)</div>
+                <a href='/' className='flex'>
+                    <span className='text-xl font-extrabold text-white'>
+                        Deloitte
+                    </span>
+                    <span className='flex self-end'>
+                        <svg
+                            className='mb-1.5 ml-0.5 h-1.5 w-1.5 fill-d-green'
+                            viewBox='0 0 100 100'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='currentColor'
+                        >
+                            <circle cx='50' cy='50' r='50' />
+                        </svg>
+                    </span>
+                </a>
                 <p className='text-xl text-d-green'>|</p>
-                <h1 className='text-xl font-bold text-white'>Neuron</h1>
+                <a href='/'>
+                    <h1 className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-xl font-bold text-transparent hover:from-pink-500 hover:to-blue-500'>
+                        Neuron
+                    </h1>
+                </a>
                 <p className='text-xl text-d-green'>|</p>
                 <h2 className='text-lg text-white'>
-                    Third-Party Risk Management (TPRM) Accelerator
+                    Third-Party Risk Management Accelerator
                 </h2>
             </div>
         </div>
