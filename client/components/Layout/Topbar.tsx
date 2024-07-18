b5f224434212afb5b06139eab29fb80a4f404333
Evan Walter <evwalter@deloitte.com>
2024-07-18 09:26:42 -0700

    refactor: sort classes in template literals

--- client/components/Layout/Topbar.tsx
+++ client/components/Layout/Topbar.tsx
@@ -1,9 +1,16 @@
 import Link from 'next/link';
+import {
+    ComputerDesktopIcon,
+    DevicePhoneMobileIcon,
+    MoonIcon,
+    SunIcon,
+} from '@heroicons/react/24/outline';
+import { tw } from '@/utils';
 
 export function Topbar(): JSX.Element {
     return (
-        <div className='bg-gradient-to-r from-d-green/50 via-indigo-950 to-d-green/50 pb-0.5 shadow-sm shadow-indigo-500/50'>
-            <div className='flex w-full items-center gap-2 bg-black px-4 py-3'>
+        <div className='flex w-full justify-around bg-gradient-to-r from-d-green/50 via-indigo-950 to-d-green/50 pb-0.5 shadow-sm shadow-indigo-500/50'>
+            <div className='flex w-full items-center gap-2 bg-white px-4 py-3 dark:bg-black'>
                 <Link href='/' className='flex'>
                     <span className='text-lg font-extrabold text-white'>
                         Deloitte
@@ -33,16 +40,36 @@ export function Topbar(): JSX.Element {
                     <span className='bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text font-bold text-transparent'>
                         .AI
                     </span>
+                    <IconButtonsTheme />
+                    {/* <IconButtonsTheme classes='size-5 stroke-zinc-300' /> */}
                 </h2>
+                <div className='flex w-full bg-pink-500'></div>
             </div>
         </div>
     );
 }
 
-export function Bar(): JSX.Element {
+function Bar(): JSX.Element {
     return (
         <p className='bg-gradient-to-t from-zinc-800 via-zinc-400 to-zinc-800 bg-clip-text text-2xl font-extrabold text-transparent'>
             |
         </p>
+    );
+}
+
+interface IconButtonsThemeProps {
+    classes: string;
+}
+
+function IconButtonsTheme(): JSX.Element {
+    const classes = tw`size-5 stroke-zinc-300`;
+
+    return (
+        <>
+            <ComputerDesktopIcon className={classes} />
+            <DevicePhoneMobileIcon className={classes} />
+            <MoonIcon className={classes} />
+            <SunIcon className={classes} />
+        </>
     );
 }
