a57dbf43a1c98f535b338fbcd4308d4c1e3b3a03
Walter <evwalter@deloitte.com>
2024-08-13 12:39:42 -0700

    chore: clean tw config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
         './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     darkMode: 'class',
     theme: {
@@ -12,9 +12,6 @@ const config: Config = {
         extend: {
             colors: {
                 'd-green': 'hsl(81, 67%, 44%)',
-                'tprm-blue-light': 'hsl(218,52%,52%)',
-                'tprm-blue-medium': 'hsl(218,53%,39%)',
-                'tprm-blue-dark': 'hsl(218,100%,19%)',
             },
         },
         fontFamily: { sans: ['"Noto Sans"'] },
