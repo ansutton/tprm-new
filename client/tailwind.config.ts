1139a1d549640aada66e29c0e4e85e6385b65e21
Walter <evwalter@deloitte.com>
2024-08-13 11:31:22 -0700

    chore: undo the shadcn changes

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from 'tailwindcss';
 
-// shadcn
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
 const config: Config = {
     content: [
         './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,83 +8,17 @@ const config: Config = {
     ],
     darkMode: 'class',
     theme: {
-        container: {
-            center: true,
-            padding: '1rem',
-            // shadcn
-            // center: true,
-            // padding: '2rem',
-            //     screens: {
-            //         '2xl': '1400px',
-            // },
-        },
+        container: { center: true, padding: '1rem' },
         extend: {
-            // shadcn
-            // borderRadius: {
-            //                 lg: `var(--radius)`,
-            //                 md: `calc(var(--radius) - 2px)`,
-            //                 sm: 'calc(var(--radius) - 4px)',
-            //             },
             colors: {
                 'd-green': 'hsl(81, 67%, 44%)',
                 'tprm-blue-light': 'hsl(218,52%,52%)',
                 'tprm-blue-medium': 'hsl(218,53%,39%)',
                 'tprm-blue-dark': 'hsl(218,100%,19%)',
-                // shacn
-                // border: 'hsl(var(--border))',
-                // input: 'hsl(var(--input))',
-                // ring: 'hsl(var(--ring))',
-                // background: 'hsl(var(--background))',
-                // foreground: 'hsl(var(--foreground))',
-                // primary: {
-                //     DEFAULT: 'hsl(var(--primary))',
-                //     foreground: 'hsl(var(--primary-foreground))',
-                // },
-                // secondary: {
-                //     DEFAULT: 'hsl(var(--secondary))',
-                //     foreground: 'hsl(var(--secondary-foreground))',
-                // },
-                // destructive: {
-                //     DEFAULT: 'hsl(var(--destructive))',
-                //     foreground: 'hsl(var(--destructive-foreground))',
-                // },
-                // muted: {
-                //     DEFAULT: 'hsl(var(--muted))',
-                //     foreground: 'hsl(var(--muted-foreground))',
-                // },
-                // accent: {
-                //     DEFAULT: 'hsl(var(--accent))',
-                //     foreground: 'hsl(var(--accent-foreground))',
-                // },
-                // popover: {
-                //     DEFAULT: 'hsl(var(--popover))',
-                //     foreground: 'hsl(var(--popover-foreground))',
-                // },
-                // card: {
-                //     DEFAULT: 'hsl(var(--card))',
-                //     foreground: 'hsl(var(--card-foreground))',
-                // },
             },
         },
         fontFamily: { sans: ['"Noto Sans"'] },
-        // shadcn
-        // sans: ['var(--font-sans)', ...fontFamily.sans],
-        // keyframes: {
-        //     'accordion-down': {
-        //         from: { height: '0' },
-        //         to: { height: 'var(--radix-accordion-content-height)' },
-        //     },
-        //     'accordion-up': {
-        //         from: { height: 'var(--radix-accordion-content-height)' },
-        //         to: { height: '0' },
-        //     },
-        // },
-        // animation: {
-        //     'accordion-down': 'accordion-down 0.2s ease-out',
-        //     'accordion-up': 'accordion-up 0.2s ease-out',
-        // },
     },
-    // shadcn
-    plugins: [require('tailwindcss-animate')],
+    plugins: [],
 };
 export default config;
