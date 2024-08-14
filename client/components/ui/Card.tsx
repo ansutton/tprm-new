90177249ddb9c09fb117763cb5308fdb773ebfbf
Walter <evwalter@deloitte.com>
2024-08-14 11:17:13 -0700

    refactor: simplify card props

--- client/components/ui/Card.tsx
+++ client/components/ui/Card.tsx
@@ -2,20 +2,18 @@ import { ReactNode } from 'react';
 import clsx from 'clsx';
 
 interface CardProps {
-    twFlex?: string;
-    twMaxWidth?: string;
+    additionalClasses?: string;
     children: ReactNode;
 }
 
 export function Card({
-    twFlex = '',
-    twMaxWidth = '',
+    additionalClasses = '',
     children,
 }: CardProps): JSX.Element {
     return (
         <div
             className={clsx(
-                `${twFlex} ${twMaxWidth}`,
+                `${additionalClasses}`,
                 'bg-zinc-50',
                 'rounded-2xl border border-indigo-200 shadow-lg shadow-indigo-200',
                 'dark:border-indigo-500/60 dark:bg-zinc-800 dark:shadow-indigo-900/40',
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -190,7 +190,7 @@ export default function Home(): JSX.Element {
 
             <div className='container mx-auto pb-5 pt-5'>
                 {screen === 'fileUpload' ? (
-                    <Card twMaxWidth='max-w-2xl'>
+                    <Card additionalClasses={tw`mx-auto max-w-2xl`}>
                         <div className='mx-auto flex flex-col gap-6 p-6'>
                             <div className='flex items-center gap-3'>
                                 <QuestionMarkCircleIcon
