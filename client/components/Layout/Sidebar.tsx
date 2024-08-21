4200ba8ee3e55f340525b98a780aa39d5b70a49d
Walter <evwalter@deloitte.com>
2024-08-21 14:46:20 -0700

    feat: improve Sidebar width

--- client/components/Layout/Sidebar.tsx
+++ client/components/Layout/Sidebar.tsx
@@ -7,62 +7,68 @@ export function Sidebar(): JSX.Element {
     const [isExpanded, setIsExpanded] = useState(true);
 
     function toggleSidebar() {
-        setIsExpanded(!isExpanded);
+        setIsExpanded((prevState) => !prevState);
     }
 
     return (
-        <div className='flex'>
-            <div
+        <div
+            className={clsx(
+                tw`absolute inset-y-0 left-0 rounded-lg px-3 pb-4 pt-20 transition-all duration-300 ease-in-out`,
+                tw`bg-zinc-200 text-black`,
+                tw`dark:bg-zinc-900 dark:text-white`,
+                isExpanded ? 'w-64' : 'w-16',
+            )}
+        >
+            <button
                 className={clsx(
-                    tw`absolute inset-y-0 left-0 space-y-6 px-4 pb-4 pt-20 transition-all duration-300 ease-in-out`,
-                    tw`bg-zinc-200 text-black`,
-                    tw`dark:bg-zinc-900 dark:text-white`,
-                    isExpanded ? 'w-64' : 'w-20',
+                    tw`rounded-lg p-2 transition-all duration-300`,
+                    isExpanded ? tw`w-full` : tw`w-10`,
+                    tw`hover:bg-zinc-300`,
+                    tw`dark:hover:bg-zinc-700`,
+                    'flex items-center justify-between',
                 )}
+                onClick={toggleSidebar}
             >
-                <button
+                <ChevronLeftIcon
                     className={clsx(
-                        tw`rounded-lg p-2 transition-all duration-300`,
-                        isExpanded ? tw`w-full` : tw`w-fit`,
+                        tw`w-6 transform transition-transform duration-300`,
+                        isExpanded
+                            ? tw`ml-auto translate-x-0 rotate-0`
+                            : tw`translate-x-0 rotate-180`,
+                        tw`stroke-zinc-700`,
+                        tw`dark:stroke-zinc-200`,
+                    )}
+                />
+            </button>
+
+            <nav className='mt-6'>
+                <button
+                    onClick={() => null}
+                    className={clsx(
+                        tw`flex items-center rounded-lg p-2`,
+                        tw`transition-all duration-300`,
+                        isExpanded ? tw`w-[232px]` : tw`w-fit`,
                         tw`hover:bg-zinc-300`,
                         tw`dark:hover:bg-zinc-700`,
-                        'flex items-center justify-between',
                     )}
-                    onClick={toggleSidebar}
                 >
-                    <ChevronLeftIcon
+                    <BoltIcon
                         className={clsx(
-                            tw`w-6 transform transition-transform duration-300`,
-                            isExpanded
-                                ? tw`ml-auto translate-x-0 rotate-0`
-                                : tw`translate-x-0 rotate-180`,
+                            tw`w-6`,
                             tw`stroke-zinc-700`,
                             tw`dark:stroke-zinc-200`,
                         )}
                     />
-                </button>
-                <nav className='mt-10'>
-                    <button
-                        onClick={() => null}
+                    <div
                         className={clsx(
-                            tw`flex items-center rounded-lg px-4 py-2.5`,
-                            tw`transition-all duration-300`,
-                            tw`hover:bg-zinc-300`,
-                            tw`dark:hover:bg-zinc-700`,
+                            tw`transition-opacity duration-300`,
+                            isExpanded ? tw`ml-2 opacity-100` : tw`opacity-0`,
                         )}
                     >
-                        <BoltIcon className='h-6 w-6' />
-                        <div
-                            className={clsx(
-                                tw`transition-opacity duration-300`,
-                                isExpanded ? 'ml-4 opacity-100' : 'opacity-0',
-                            )}
-                        >
-                            {isExpanded && <span>Home</span>}
-                        </div>
-                    </button>
-                </nav>
-            </div>
+                        {isExpanded ? 'Home' : null}
+                    </div>
+                </button>
+            </nav>
         </div>
     );
 }
