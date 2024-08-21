12ae3542beaa9bf2b03272000018b6ba43672a30
Walter <evwalter@deloitte.com>
2024-08-21 13:21:59 -0700

    feat: (WIP) style Sidebar

--- client/components/Layout/Sidebar.tsx
+++ client/components/Layout/Sidebar.tsx
@@ -3,46 +3,46 @@ import { BoltIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import { tw } from '@/utils';
 
-interface SidebarProps {
-    isSidebarExpanded: boolean;
-    setIsSidebarExpanded: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-export function Sidebar({
-    isSidebarExpanded,
-    setIsSidebarExpanded,
-}: SidebarProps): JSX.Element {
-    const [isExpanded, setIsExpanded] = useState(true);
+export function Sidebar(): JSX.Element {
+    const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
 
     function toggleSidebar() {
-        setIsExpanded(!isExpanded);
+        setIsSidebarExpanded(!isSidebarExpanded);
     }
 
     return (
         <div className='flex'>
             <div
                 className={clsx(
-                    tw`absolute inset-y-0 left-0 space-y-6 p-4 transition-all duration-300 ease-in-out`,
-                    tw`text-white dark:bg-zinc-800`,
-                    isExpanded ? 'w-64' : 'w-20',
+                    tw`absolute inset-y-0 left-0 space-y-6 px-4 pb-4 pt-20 transition-all duration-300 ease-in-out`,
+                    tw`bg-zinc-200 text-black`,
+                    tw`dark:bg-zinc-900 dark:text-white`,
+                    isSidebarExpanded ? 'w-64' : 'w-20',
                 )}
             >
-                <button
-                    className='text-white focus:outline-none'
-                    onClick={toggleSidebar}
-                >
-                    {isExpanded ? 'Collapse' : 'Expand'}
+                <button className='focus:outline-none' onClick={toggleSidebar}>
+                    {isSidebarExpanded ? 'Collapse' : 'Expand'}
                 </button>
                 <nav className='mt-10'>
                     <button
                         onClick={() => null}
                         className={clsx(
-                            tw`flex items-center rounded px-4 py-2.5 transition duration-200`,
+                            tw`flex items-center rounded-lg px-4 py-2.5`,
+                            tw`hover:bg-zinc-300`,
                             tw`dark:hover:bg-zinc-700`,
                         )}
                     >
                         <BoltIcon className='h-6 w-6' />
-                        {isExpanded ? <span className='ml-4'>Home</span> : null}
+                        <div
+                            className={clsx(
+                                tw`delay-2000 transition-opacity duration-500`,
+                                isSidebarExpanded ? 'opacity-100' : 'opacity-0',
+                            )}
+                        >
+                            {isSidebarExpanded ? (
+                                <span className={clsx(tw`ml-4`)}>Home</span>
+                            ) : null}
+                        </div>
                     </button>
                 </nav>
             </div>
@@ -53,7 +53,7 @@ export function Sidebar({
                     className='text-gray-800 focus:outline-none'
                     onClick={toggleSidebar}
                 >
-                    {isExpanded ? 'Hide Sidebar' : 'Show Sidebar'}
+                    {isSidebarExpanded ? 'Hide Sidebar' : 'Show Sidebar'}
                 </button>
                 <h1>Main Content</h1>
                 <p>Your main content goes here.</p>
