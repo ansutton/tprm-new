dc0d97030e5a93c2c704a7bfe9b2e147f417d500
Walter <evwalter@deloitte.com>
2024-08-23 11:35:30 -0700

    feat: start to build table

--- client/components/Layout/DetailedAnalysisNew.tsx
+++ client/components/Layout/DetailedAnalysisNew.tsx
@@ -1,7 +1,31 @@
 import clsx from 'clsx';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import { Card, Heading, Tooltip } from '@/components';
 import { LlmResponse } from '@/types';
 import { tw } from '@/utils';
+
+interface Product {
+    id: string;
+    name: string;
+    category: string;
+    quantity: number;
+}
+
+const products = [
+    {
+        id: '1',
+        name: 'Shirt',
+        category: 'Clothing',
+        quantity: 5,
+    },
+    {
+        id: '2',
+        name: 'Watch',
+        category: 'Accessories',
+        quantity: 6,
+    },
+];
 
 interface DetailedAnalysisNewProps {
     excelData: any[][];
@@ -18,6 +42,11 @@ export function DetailedAnalysisNew({
         <>
             <div>DetailedAnalysisNew</div>
             <div>PrimeReactExample</div>
+            <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
+                <Column field='name' header='Name' />
+                <Column field='category' header='Category' />
+                <Column field='quantity' header='Quantity' />
+            </DataTable>
         </>
     );
 }
