772347a6f076040843faf156f42e07fce3154011
Walter <evwalter@deloitte.com>
2024-08-27 10:32:11 -0700

    feat: implement expand all button

--- client/components/Layout/DetailedAnalysisNew.tsx
+++ client/components/Layout/DetailedAnalysisNew.tsx
@@ -1,5 +1,5 @@
 import { Fragment, ReactNode, useState } from 'react';
-import { ChevronDownIcon } from '@heroicons/react/16/solid';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import {
     createColumnHelper,
     flexRender,
@@ -9,7 +9,7 @@ import {
     useReactTable,
 } from '@tanstack/react-table';
 import clsx from 'clsx';
-import { Tooltip } from '@/components';
+import { Button, Tooltip } from '@/components';
 import { LlmResponse } from '@/types';
 import { truncate, tw } from '@/utils';
 
@@ -45,9 +45,9 @@ function TableHeader({
 }
 
 const iconClasses = clsx(
-    tw`size-6 stroke-0`,
-    tw`text-zinc-600`,
-    tw`dark:text-zinc-400`,
+    tw`size-5 stroke-2`,
+    tw`stroke-zinc-600`,
+    tw`dark:stroke-zinc-400`,
     tw`transform transition-transform duration-200`,
 );
 
@@ -60,7 +60,7 @@ const columns = [
         cell: ({ row }) => (
             <button
                 className={clsx(
-                    tw`rounded-full p-1 transition-all`,
+                    tw`rounded-full p-2 transition-all`,
                     tw`group-hover:bg-zinc-300`,
                     tw`dark:group-hover:bg-zinc-800`,
                 )}
@@ -194,120 +194,173 @@ export function DetailedAnalysisNew({
     });
 
     return (
-        <div
-            className={clsx(
-                tw`overflow-x-auto`,
-                tw`bg-zinc-100 dark:bg-zinc-950`,
-                tw`rounded-lg border border-zinc-300 dark:border-zinc-600`,
-                tw`drop-shadow-md`,
-            )}
-        >
-            <table className='w-full dark:text-zinc-100'>
-                <thead>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <tr key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => (
-                                <th
-                                    key={header.id}
-                                    className={clsx(
-                                        tw`whitespace-nowrap p-3 text-left text-sm`,
-                                        tw`border-b border-zinc-300 dark:border-zinc-600`,
-                                        header.id === 'expander' && tw`w-5`,
-                                        header.id === 'questionNumber' &&
-                                            tw`w-5`,
-                                        header.id === 'question' && tw`w-fit`,
-                                        header.id ===
-                                            'thirdPartyResponsePreview' &&
-                                            tw`w-1/6`,
-                                        header.id ===
-                                            'evidenceAnalysisPreview' &&
-                                            tw`w-1/6`,
-                                        header.id === 'answersAlign' &&
-                                            tw`w-1/12`,
-                                        header.id === 'confidenceScore' &&
-                                            tw`w-1/12`,
-                                        header.id === 'similarityScore' &&
-                                            tw`w-1/12`,
-                                        header.id === 'citationPreview' &&
-                                            tw`w-1/12`,
-                                    )}
-                                >
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                              header.column.columnDef.header,
-                                              header.getContext(),
-                                          )}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
+        <>
+            <button
+                onClick={() => table.toggleAllRowsExpanded()}
+                className={clsx(
+                    tw`w-32`,
+                    tw`relative ml-auto`,
+                    tw`w-fit rounded-lg p-2`,
+                    tw`text-indigo-600 dark:text-indigo-400`,
+                    tw`transition-all`,
+                    tw`hover:cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800`,
+                    tw`flex items-center gap-1.5`,
+                )}
+            >
+                {table.getIsAllRowsExpanded() ? (
+                    <>
+                        <ChevronDownIcon
+                            className={clsx(
+                                tw`size-5 stroke-2`,
+                                tw`stroke-indigo-600 dark:stroke-indigo-400`,
+                                tw`transform transition-transform duration-200`,
+                            )}
+                        />
+                        <span className={clsx(tw`text-sm font-bold`, tw``)}>
+                            Contract All
+                        </span>
+                    </>
+                ) : (
+                    <>
+                        <ChevronDownIcon
+                            className={clsx(
+                                tw`-rotate-90`,
+                                tw`size-5 stroke-2`,
+                                tw`stroke-indigo-600 dark:stroke-indigo-400`,
+                                tw`transform transition-transform duration-200`,
+                            )}
+                        />
+                        <span
+                            className={clsx(
+                                tw`text-sm font-bold`,
+                                tw`text-indigo-600 dark:text-indigo-400`,
+                            )}
+                        >
+                            Expand All
+                        </span>
+                    </>
+                )}
+            </button>
 
-                <tbody
-                    className={clsx(
-                        tw`divide-y divide-zinc-300 dark:divide-zinc-600`,
-                    )}
-                >
-                    {table.getRowModel().rows.map((row) => (
-                        <Fragment key={row.id}>
-                            <tr
-                                onClick={() => row.toggleExpanded()}
-                                className={clsx(
-                                    tw`group`,
-                                    tw`transition-all duration-200 ease-out`,
-                                    tw`hover:cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-900`,
-                                    row.getIsExpanded() &&
-                                        tw`bg-zinc-200 dark:bg-zinc-900`,
-                                )}
-                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <td
-                                        key={cell.id}
+            <div
+                className={clsx(
+                    tw`overflow-x-auto`,
+                    tw`bg-zinc-100 dark:bg-zinc-950`,
+                    tw`rounded-lg border border-zinc-300 dark:border-zinc-600`,
+                    tw`drop-shadow-md`,
+                )}
+            >
+                <table className='w-full dark:text-zinc-100'>
+                    <thead>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <th
+                                        key={header.id}
                                         className={clsx(
-                                            tw`p-3 text-sm`,
-                                            (cell.column.id ===
-                                                'thirdPartyResponsePreview' ||
-                                                cell.column.id ===
-                                                    'evidenceAnalysisPreview') &&
-                                                tw`select-none`,
+                                            tw`whitespace-nowrap p-3 text-left text-sm`,
+                                            tw`border-b border-zinc-300 dark:border-zinc-600`,
+                                            header.id === 'expander' && tw`w-5`,
+                                            header.id === 'questionNumber' &&
+                                                tw`w-5`,
+                                            header.id === 'question' &&
+                                                tw`w-fit`,
+                                            header.id ===
+                                                'thirdPartyResponsePreview' &&
+                                                tw`w-1/6`,
+                                            header.id ===
+                                                'evidenceAnalysisPreview' &&
+                                                tw`w-1/6`,
+                                            header.id === 'answersAlign' &&
+                                                tw`w-1/12`,
+                                            header.id === 'confidenceScore' &&
+                                                tw`w-1/12`,
+                                            header.id === 'similarityScore' &&
+                                                tw`w-1/12`,
+                                            header.id === 'citationPreview' &&
+                                                tw`w-1/12`,
                                         )}
                                     >
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext(),
-                                        )}
-                                    </td>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext(),
+                                              )}
+                                    </th>
                                 ))}
                             </tr>
-                            {row.getIsExpanded() && (
-                                <>
-                                    <ExpandedRow
-                                        content={
-                                            row.original.thirdPartyResponseFull
-                                        }
-                                        row={row}
-                                        title={'Third Party Response'}
-                                    />
-                                    <ExpandedRow
-                                        content={
-                                            row.original.evidenceAnalysisFull
-                                        }
-                                        row={row}
-                                        title={'Evidence Analysis'}
-                                    />
-                                    <ExpandedRow
-                                        content={row.original.citationFull}
-                                        row={row}
-                                        title={'Citation'}
-                                    />
-                                </>
-                            )}
-                        </Fragment>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                        ))}
+                    </thead>
+
+                    <tbody
+                        className={clsx(
+                            tw`divide-y divide-zinc-300 dark:divide-zinc-600`,
+                        )}
+                    >
+                        {table.getRowModel().rows.map((row) => (
+                            <Fragment key={row.id}>
+                                <tr
+                                    onClick={() => row.toggleExpanded()}
+                                    className={clsx(
+                                        tw`group`,
+                                        tw`transition-all duration-200 ease-out`,
+                                        tw`hover:cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-900`,
+                                        row.getIsExpanded() &&
+                                            tw`bg-zinc-200 dark:bg-zinc-900`,
+                                    )}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <td
+                                            key={cell.id}
+                                            className={clsx(
+                                                tw`p-3 text-sm`,
+                                                (cell.column.id ===
+                                                    'thirdPartyResponsePreview' ||
+                                                    cell.column.id ===
+                                                        'evidenceAnalysisPreview') &&
+                                                    tw`select-none`,
+                                            )}
+                                        >
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext(),
+                                            )}
+                                        </td>
+                                    ))}
+                                </tr>
+                                {row.getIsExpanded() && (
+                                    <>
+                                        <ExpandedRow
+                                            content={
+                                                row.original
+                                                    .thirdPartyResponseFull
+                                            }
+                                            row={row}
+                                            title={'Third Party Response'}
+                                        />
+                                        <ExpandedRow
+                                            content={
+                                                row.original
+                                                    .evidenceAnalysisFull
+                                            }
+                                            row={row}
+                                            title={'Evidence Analysis'}
+                                        />
+                                        <ExpandedRow
+                                            content={row.original.citationFull}
+                                            row={row}
+                                            title={'Citation'}
+                                        />
+                                    </>
+                                )}
+                            </Fragment>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
