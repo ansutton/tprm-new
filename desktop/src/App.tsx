11447e7ed4c65109e82dfa96a947ef91dfd95fb8
Walter <evwalter@deloitte.com>
2024-08-28 17:02:20 -0700

    config: set up relative imports

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,8 +1,14 @@
 // import { useState } from "react";
+import { MyComponent } from "@/components";
 import "./App.css";
 
 export default function App() {
     // const [count, setCount] = useState(0);
 
-    return <div>App</div>;
+    return (
+        <>
+            <div>App</div>
+            <MyComponent />
+        </>
+    );
 }
