7e98add3d570aa65ce75d16fc70c40e8973727c7
Walter <evwalter@deloitte.com>
2024-09-06 19:46:55 -0700

    feat: add commas between pages

--- desktop/src/components/Layout/DetailedAnalysis.tsx
+++ desktop/src/components/Layout/DetailedAnalysis.tsx
@@ -279,10 +279,24 @@ export function DetailedAnalysis({
                                 `analysis_${index}`
                             ]?.pages?.map((pageNumber, j) => (
                                 <p key={j} className='flex'>
-                                    <span className=''>{pageNumber},</span>
+                                    <span>{pageNumber}</span>
+                                    {j + 1 !==
+                                        llmResponse?.analyses[
+                                            `analysis_${index}`
+                                        ]?.pages?.length &&
+                                        (llmResponse?.analyses[
+                                            `analysis_${index}`
+                                        ]?.pages?.length ?? 0) > 1 &&
+                                        ', '}
                                 </p>
                             )),
                         )}
+                        {/* {[4, 2].map((item, j)=> (
+                            <div key={j}>
+                                <span className="">{item}</span>
+                                {(j + 1) !== [4, 2].length && (([4, 2]?.length ?? 0) > 1) && ', '}
+                            </div>
+                        ))} */}
                     </div>
                 ),
             }));
