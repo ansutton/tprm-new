a9735ce4b3ea16acafae7b583227ac17f8bf4df7
Walter <evwalter@deloitte.com>
2024-09-11 15:30:13 -0700

    feat: add tooltip to Evidence Answered

--- desktop/src/components/OverviewCards/EvidenceAnswered.tsx
+++ desktop/src/components/OverviewCards/EvidenceAnswered.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { Card, ProgressBarBase } from '@/components';
+import { Card, ProgressBarBase, Tooltip } from '@/components';
 import { ProgressBarBaseProps } from '@/types';
 
 interface EvidenceAnsweredProps extends ProgressBarBaseProps {
@@ -17,8 +17,13 @@ export function EvidenceAnswered({
         <Card>
             <div className='flex items-center gap-2'>
                 {startIcon}
-                <h4 className='mb-3 w-full text-lg font-bold opacity-80'>
+                <h4 className='mb-4 inline w-full text-lg font-bold opacity-80'>
                     {title}
+                    <span className='relative ml-1.5 inline-flex'>
+                        <span className='absolute -top-4'>
+                            <Tooltip twStroke='stroke-2'>{`% of questions analyzed where the AI was able to reference a citation justifying its response`}</Tooltip>
+                        </span>
+                    </span>
                 </h4>
             </div>
             <p className='mb-1 text-3xl font-bold'>{progressPercentage}%</p>
