aaf0234d656c52b3b5e4cae87755cd1ecc59ef48
Walter <evwalter@deloitte.com>
2024-09-11 15:25:03 -0700

    feat: comment out Confidence Score from Overview screen

--- desktop/src/components/Layout/Overview.tsx
+++ desktop/src/components/Layout/Overview.tsx
@@ -48,13 +48,13 @@ export function Overview({
                     llmResponse={llmResponse}
                     questionsData={questionsData}
                 />
-                <ConfidenceScore
+                {/* <ConfidenceScore
                     llmResponse={llmResponse}
                     questionsData={questionsData}
                     title='AI Confidence Score'
                     tooltipContent={`How confident is the AI is when determining whether its responses align with the third party responses? (0-100%, aggregated)`}
                     subtitle='Aggregate'
-                />
+                /> */}
                 <AnswersAlign
                     llmResponse={llmResponse}
                     questionsData={questionsData}
