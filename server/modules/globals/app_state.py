33d0985f428b6e18aba8bed16465588fc6ee5dd2
Wallace <jacwallace@deloitte.com>
2024-09-12 14:30:35 -0500

    added embeddings_total

--- server/modules/globals/app_state.py
+++ server/modules/globals/app_state.py
@@ -4,14 +4,18 @@ class AppState:
     models_pulled = False
     is_complete = False
     number_of_questions = 0
+    embeddings_count = 0
+    embeddings_total = 0
     analyses: dict[str, object] = {}
 
     def to_dict(self):
         return {
             "number_of_questions": self.number_of_questions,
+            "embeddings_count": self.embeddings_count,
+            "embeddings_total": self.embeddings_total,
             "models_pulled": self.models_pulled,
             "is_complete": self.is_complete,
-            "analyses": self.analyses
+            "analyses": self.analyses,
         }
 
 class AnalysisData:
