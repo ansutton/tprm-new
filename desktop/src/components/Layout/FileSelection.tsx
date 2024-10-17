883a89a17a11911897d7d87c1da2d721fad6f0d7
Walter <evwalter@deloitte.com>
2024-10-17 15:08:38 -0700

    feat: implement ability to upload single or multiple files

--- desktop/src/components/Layout/FileSelection.tsx
+++ desktop/src/components/Layout/FileSelection.tsx
@@ -1,9 +1,39 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
+// TODO: extract to three FileInput components, and put them all in here.
 export function FileSelection(): JSX.Element {
+    const [fileSelection, setFileSelection] = useState<File | null>(null);
+    const [filesSelection, setFilesSelection] = useState<File[] | null>(null);
+
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    const buttonText = `Select File${filesSelection ? 's' : ''}`;
+
+    function handleChange(
+        e: React.ChangeEvent<HTMLInputElement>,
+        singleOrMultiple: 'single' | 'multiple',
+    ) {
+        const files = e.target.files;
+        if (files) {
+            if (singleOrMultiple === 'single') {
+                setFileSelection(files[0]);
+            } else if (singleOrMultiple === 'multiple') {
+                setFilesSelection([...files]);
+            }
+        }
+    }
+
     return (
-        <>
-            <>FileSelection</>
-        </>
+        <div className='relative'>
+            <input
+                type='file'
+                className='absolute h-0 w-0 opacity-0'
+                ref={fileInputRef}
+                onChange={(e) => handleChange(e, 'multiple')}
+            />
+            <button className='' onClick={() => fileInputRef.current?.click()}>
+                {buttonText}
+            </button>
+        </div>
     );
 }
