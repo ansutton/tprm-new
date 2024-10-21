c8fc8474c28516c5209d24be7ccf62addaa18b6b
Walter <evwalter@deloitte.com>
2024-10-21 11:56:52 -0700

    feat: change footnote asterisks in table headers to indigo color; chore: reorder constants and components

--- desktop/src/components/Layout/DetailedAnalysis.tsx
+++ desktop/src/components/Layout/DetailedAnalysis.tsx
@@ -23,19 +23,9 @@ import {
 } from '@/types';
 import { displayScore, handleAnswersAlign, truncate, tw } from '@/utils';
 
-function TableHeader({
-    additionalClasses = '',
-    headerContent,
-    infoContent,
-}: TableHeaderProps): JSX.Element {
-    return (
-        <div className={clsx(additionalClasses, tw`flex items-center gap-1.5`)}>
-            <span>{headerContent}</span>
-            {infoContent && <Tooltip>{infoContent}</Tooltip>}
-        </div>
-    );
-}
-
+/**
+ * Constants
+ */
 const iconClasses = clsx(
     tw`size-5 stroke-2`,
     tw`stroke-zinc-600 dark:stroke-zinc-400`,
@@ -118,7 +108,12 @@ const columns = [
     columnHelper.accessor('aiAnalysisPreview', {
         header: () => (
             <TableHeader
-                headerContent='AI Response*'
+                headerContent={
+                    <>
+                        <span>AI Response</span>
+                        <Asterisk />
+                    </>
+                }
                 infoContent={`The AI's response to the question`}
             />
         ),
@@ -127,7 +122,12 @@ const columns = [
     columnHelper.accessor('citationsPreview', {
         header: () => (
             <TableHeader
-                headerContent='Citation(s)*'
+                headerContent={
+                    <>
+                        <span>Citation(s)</span>
+                        <Asterisk />
+                    </>
+                }
                 infoContent={`Page #(s) with relevant excerpt(s) from the provided evidence document(s) used to generate the AI's response`}
             />
         ),
@@ -171,6 +171,25 @@ const columns = [
     // }),
 ];
 
+/**
+ * Components
+ */
+function TableHeader({
+    additionalClasses = '',
+    headerContent,
+    infoContent,
+}: TableHeaderProps): JSX.Element {
+    return (
+        <div className={clsx(additionalClasses, tw`flex items-center gap-1.5`)}>
+            <span>{headerContent}</span>
+            {infoContent && <Tooltip>{infoContent}</Tooltip>}
+        </div>
+    );
+}
+
+function Asterisk(): JSX.Element {
+    return <span className='text-indigo-500/85 dark:text-indigo-400'>*</span>;
+}
 interface DetailedAnalysisProps {
     excelData: any[][];
     llmResponse: LlmResponse;
