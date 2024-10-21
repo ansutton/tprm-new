6cbc3929a6843719da7641c1794113fcf73aa05a
Walter <evwalter@deloitte.com>
2024-10-21 14:56:33 -0700

    chore: organize constants

--- desktop/src/constants/constants.ts
+++ desktop/src/constants/constants.ts
@@ -1,6 +1,9 @@
-export const tableFootnoteText: string = `*The "AI Response" and "Citation(s)" content was produced by Generative Artificial Intelligence (Gen AI).`;
-
 /**
  * File Selection
  */
 export const confirmDeletionMessage: string = `Are you sure you would like to remove this file?`;
+
+/**
+ * Detailed Analysis
+ */
+export const tableFootnoteText: string = `*The "AI Response" and "Citation(s)" content was produced by Generative Artificial Intelligence (Gen AI).`;
