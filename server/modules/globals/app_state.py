14026fd5d92d3812b334cf44354a480d6f5fd6f2
Walter, Evan <evwalter@deloitte.com>
2024-11-04 11:19:47 -0800

    chore: reorder app state and types

--- server/modules/globals/app_state.py
+++ server/modules/globals/app_state.py
@@ -2,11 +2,11 @@ from abc import ABC, abstractmethod
 
 class AppState:
     models_pulled = False
-    is_complete = False
     number_of_questions = 0
     embeddings_count = 0
     embeddings_total = 0
     analyses: dict[str, object] = {}
+    is_complete = False
 
     def to_dict(self):
         return {
@@ -14,14 +14,14 @@ class AppState:
             "embeddings_count": self.embeddings_count,
             "embeddings_total": self.embeddings_total,
             "models_pulled": self.models_pulled,
-            "is_complete": self.is_complete,
             "analyses": self.analyses,
+            "is_complete": self.is_complete,
         }
 
 class AnalysisData:
     question = ""
-    ai_analysis = ""
     tp_response = ""
+    ai_analysis = ""
     citations = ""
     pages = ""
     tp_confidence_score = 0
